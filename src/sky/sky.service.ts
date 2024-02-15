d7ea5c798e4fc66a4314098166c39bc013988701
Markos <markos.torosyan@outlook.com>
2024-02-15 20:58:21 +0400

    part 2

--- src/sky/sky.service.ts
+++ src/sky/sky.service.ts
@@ -5,6 +5,7 @@ import { FilteredSkyDto } from './dto/filtered-sky.dto';
 
 @Injectable()
 export class SkyService {
+  objectData = { data: [] };
   constructor(private httpService: HttpService) {}
 
   async getFlights(filteredSkyDto: FilteredSkyDto) {
@@ -52,37 +53,126 @@ export class SkyService {
         },
       ),
     );
+
     const result = secondRequest.data.content.results.itineraries;
 
-    const findingKey = this.findKey(result, 'pricingOptions');
+    this.parseData(result);
+    const legs = secondRequest.data.content.results.legs;
+    const segments = secondRequest.data.content.results.segments;
+    const places = secondRequest.data.content.results.places;
 
-    const mappingFind = findingKey.map((obj) => {
-      return obj.items;
-    });
-
-    const extractedData = mappingFind.flatMap((item) => {
-      if (item[0]?.price && item[0]?.price?.amount) {
-        const deepLink = item[0].deepLink;
-        const priceAmount = item[0].price.amount;
-        return { deepLink, priceAmount };
-      }
-    });
-
-    const finalResult = extractedData.map((a) => {
-      return { price: Number(a.priceAmount) / 1000, link: a.deepLink };
-    });
-
-    return finalResult;
+    this.objectData.data.forEach((leg) => this.findLegsById(leg.legs, legs));
+    this.objectData.data.forEach((segment) =>
+      segment.segment.forEach((f) => this.findSegmentById(f, segments)),
+    );
+    this.objectData.data.forEach((leg) =>
+      this.findCountry(
+        leg.legs.originPlaceId,
+        leg.legs.destinationPlaceId,
+        places,
+      ),
+    );
+    // const {} = this.objectData.data;
+    return places;
+    return this.objectData;
+    // return segments;
   }
 
-  findKey(obj, targetKey) {
+  parseData(obj) {
     for (const key in obj) {
-      if (key === targetKey) {
-        return obj[key];
-      } else if (typeof obj[key] === 'object') {
-        const result = this.findKey(obj[key], targetKey);
-        if (result !== undefined) {
-          return result;
+      const arr = obj[key].pricingOptions;
+      for (const beforeItem of arr) {
+        for (const item of beforeItem.items) {
+          this.objectData.data.push({
+            link: item.deepLink,
+            price: item.price?.amount / 1000,
+            segment: item.fares.map((s) => s.segmentId),
+            legs: obj[key].legIds[0],
+          });
+        }
+      }
+    }
+  }
+
+  // parseData(obj) {
+  //   const objectData = { data: [] };
+  //   for (const key in obj) {
+  //     const arr = obj[key].pricingOptions;
+  //     arr.map((item) => {
+  //       item.items.map((b) => {
+  //         objectData.data.push({
+  //           link: b.deepLink,
+  //           price: b.price?.amount / 1000,
+  //           segment: b.fares.map((s) => s.segmentId),
+  //           legs: obj[key].legIds[0],
+  //         });
+  //       });
+  //     });
+  //   }
+  //   return objectData;
+  // }
+
+  findCountry(originPlaceId, destinationPlaceId, places) {
+    for (const key in places) {
+      if (key === originPlaceId) {
+        console.log(key, originPlaceId);
+      }
+    }
+  }
+
+  findLegsById(legId, data) {
+    for (const key in data) {
+      if (key === legId) {
+        for (const leg of this.objectData.data) {
+          const {
+            originPlaceId,
+            destinationPlaceId,
+            departureDateTime,
+            arrivalDateTime,
+            durationInMinutes,
+            stopCount,
+            marketingCarrierIds,
+            operatingCarrierIds,
+          } = data[key];
+          leg.legs = {
+            originPlaceId,
+            destinationPlaceId,
+            departureDateTime,
+            arrivalDateTime,
+            durationInMinutes,
+            stopCount,
+            marketingCarrierIds,
+            operatingCarrierIds,
+          };
+        }
+      }
+    }
+  }
+
+  findSegmentById(segmentId, data) {
+    for (const key in data) {
+      if (key === segmentId) {
+        for (const s of this.objectData.data) {
+          const {
+            originPlaceId,
+            destinationPlaceId,
+            departureDateTime,
+            arrivalDateTime,
+            durationInMinutes,
+            marketingFlightNumber,
+            marketingCarrierId,
+            operatingCarrierId,
+          } = data[key];
+          s.segmentData = {
+            originPlaceId,
+            destinationPlaceId,
+            departureDateTime,
+            arrivalDateTime,
+            durationInMinutes,
+            marketingFlightNumber,
+            marketingCarrierId,
+            operatingCarrierId,
+          };
         }
       }
     }
